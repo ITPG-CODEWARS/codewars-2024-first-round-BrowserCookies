c67b9ca1ff60d7b47dd621fb80be7e225b781b72
Nikolai Sharlopov <nikolaisharlopov@gmail.com>
2024-10-23 21:53:05 +0300

    fourth commit

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const toTopBtn = document.querySelector(".to-top-a");
+const bottomLabel = document.querySelector(".bottom-label");
 
 window.onscroll = () => {
-  if (window.scrollY > 100) {
-    toTopBtn.style.display = "block";
+  if (window.scrollY > 150) {
+    toTopBtn.style.opacity = "1";
+    toTopBtn.style.bottom = "3vw";
+    toTopBtn.style.pointerEvents = "all";
+
+    bottomLabel.style.opacity = "0";
   } else {
-    toTopBtn.style.display = "none";
+    toTopBtn.style.bottom = "0.5vw";
+    toTopBtn.style.opacity = "0";
+    toTopBtn.style.pointerEvents = "none";
+
+    bottomLabel.style.opacity = "1";
   }
 };
